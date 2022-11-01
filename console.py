905df37a3bb284296e04e2158156a6eb03261a48
Ashleylegit <Ashleybk876@gmail.com>
2022-11-01 02:11:40 -0500

    Update console.py

--- console.py
+++ console.py
@@ -16,13 +16,14 @@ from shlex import split
 # A global list that specifies the menu of the command interpreter
 CLASSES = ["BaseModel","User","City","Place","State","Amenity","Review"]
 
+
 def parse(arg):
-    """A function that parses the command
-     
-		PARAMETERS
+    """ A function that parses the command
+
+        PARAMETERS
             arg: string containing commands
     """
-    
+
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
     if curly_braces is None:
@@ -39,13 +40,15 @@ def parse(arg):
         retl.append(curly_braces.group())
         return retl
 
+
 def is_missing(args):
-    """A function that checks if the class name is missing or doesn't exist
-     
+    """ A function that checks if the class name is missing or doesn't
+        exist
+    
 		PARAMETERS
             args: string containing commands
     """
-    
+
     arg_list = parse(args)
 
     if len(arg_list) == 0:
@@ -55,29 +58,29 @@ def is_missing(args):
     else:
         return arg_list
 
+
 class HBNBCommand(cmd.Cmd):
     """A class that defines the command interpreter"""
     
-    prompt = "(hbnb) "
+    prompt = "(hbnb)"
     storage = models.storage
 
     def emptyline(self):
         """ A function that defines a command to be executed
             when an empty line + <ENTER> key is pressed
         """
-        
         pass
 
     def default(self, arg):
-        """ A function that defines the default behaviour for the 
-            cmd module when an input is invalid
-            
+        """ A function that defines the default behaviour for
+            the cmd module when an input is invalid
+        
             PARAMETERS
-            self: the constructor variable
-            arg: string containing commands
+                self: the constructor variable
+                arg: string containing commands
         """
         
-        action_map = {
+        action_m0ap = {
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
@@ -100,49 +103,53 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     def do_EOF(self, argv):
-        """A function that executes the EOF menu command to exit the program
+        """ A function that executes the EOF menu command to 
+            exit the program
                     
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
-        
+
         print("")
         return True
 
     def do_quit(self, argv):
-        """A function that executes the Quit menu command to exit the program
+        """ A function that executes the Quit menu command to exit
+            the program
                     
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
         
         return True
 
     def do_create(self, argv):
-        """ A function that executes the Create menu command to create a new instance
-            of the BaseModel class, save it (to a JSON file) and print the id
+        """ A function that executes the Create menu command to create
+            a new instance of the BaseModel class, save it (to a 
+            JSON file) and print the id
                     
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
-        
+
         args = is_missing(argv)
         if args:
             print(eval(args[0])().id)
             self.storage.save()
 
     def do_show(self, argv):
-        """ A function that executes the Show menu command and prints the string 
-            representation of an instance based on the class name and id
+        """ A function that executes the Show menu command and prints the
+            string representation of an instance based on the class name
+            and id
                             
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
-        
+
         args = is_missing(argv)
         if args:
             if len(args) != 2:
@@ -155,12 +162,13 @@ class HBNBCommand(cmd.Cmd):
                     print(self.storage.all()[key])
 
     def do_all(self, argv):
-        """ A function that executes the All menu command and prints all string 
-            representation of all instances based on or not on the class name
+        """ A function that executes the All menu command and prints all 
+            string representation of all instances based on or not on the 
+            class name
                             
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
         
         arg_list = split(argv)
@@ -175,13 +183,13 @@ class HBNBCommand(cmd.Cmd):
                        if arg_list[0] in str(obj)])
 
     def do_destroy(self, argv):
-        """ A function that execuites the Destroy menu command and deletes a class 
-            instance based on the name and given id and saves the changes to the 
-            JSON file
+        """ A function that execuites the Destroy menu command and deletes
+            a class instance based on the name and given id and saves the 
+            changes to the JSON file
                             
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
         
         arg_list = is_missing(argv)
@@ -197,13 +205,13 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
     def do_update(self, argv):
-        """ A function that executes the Update menu command and updates an instance 
-            based on the class name and id by adding or updating attribute and saves 
-            the changes the JSON file
+        """ A function that executes the Update menu command and updates 
+            an instance based on the class name and id by adding or updating 
+            attribute and saves the changes the JSON file
                             
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
         
         arg_list = is_missing(argv)
@@ -230,12 +238,12 @@ class HBNBCommand(cmd.Cmd):
             self.storage.save()
 
     def do_count(self, arg):
-        """ A function that executes the Count menu command and retrieve the number of 
-            instances of a class
+        """ A function that executes the Count menu command and retrieve the 
+            number of instances of a class
                             
             PARAMETERS
-            self: the constructor variable
-            argv: string containing commands
+                self: the constructor variable
+                argv: string containing commands
         """
         
         arg1 = parse(arg)
@@ -247,4 +255,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
